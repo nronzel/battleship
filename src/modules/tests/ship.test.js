f0a8f7c0621d0598af518a9cdc6e26729d4158df
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-18 12:31:55 -0500

    added test for ship factory

---
+++ src/modules/tests/ship.test.js
@@ -0,0 +1,16 @@
+import { Ship } from "../ship.js";
+
+test("Hit always increments by 1", () => {
+  let ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  expect(ship.numberOfHits).toBe(2);
+});
+
+test("Ship will sink", () => {
+  let ship = Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
