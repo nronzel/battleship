4f5db494367640e975ca7c6b853d2ea17e553db7
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-18 16:05:05 -0500

    added test for new method

--- src/modules/tests/ship.test.js
+++ src/modules/tests/ship.test.js
@@ -1,16 +1,31 @@
 import { Ship } from "../ship.js";
 
 test("Hit always increments by 1", () => {
-  let ship = Ship(3);
+  let ship = Ship("test", 3);
   ship.hit();
   ship.hit();
   expect(ship.numberOfHits).toBe(2);
 });
 
 test("Ship will sink", () => {
-  let ship = Ship(3);
+  let ship = Ship("test", 3);
   ship.hit();
   ship.hit();
   ship.hit();
   expect(ship.isSunk()).toBe(true);
+});
+
+test("Ship accepts coords", () => {
+  let ship = Ship("test", 3);
+  ship.setLocation([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ]);
+  expect(ship.location).toHaveLength(3);
+  expect(ship.location).toEqual([
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ]);
 });
