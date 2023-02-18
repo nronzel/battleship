0b943e6c64bf97501b5d19695b6a29bf253cfe34
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-18 16:04:02 -0500

    modified ship factory
    added a location property
    added a method to set the location

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,9 +1,10 @@
-const Ship = (length) => {
+const Ship = (name, length) => {
   let numberOfHits = 0;
   let ship = Object.create(shipActions);
+  ship.name = name;
   ship.length = length;
   ship.numberOfHits = numberOfHits;
-
+  ship.location = [];
   return ship;
 };
 
@@ -11,8 +12,13 @@ const shipActions = {
   hit() {
     return this.numberOfHits++;
   },
+
   isSunk() {
-    return this.numberOfHits === this.length ? true : false;
+    return this.numberOfHits === this.length;
+  },
+
+  setLocation(coords) {
+    return (this.location = [...coords]);
   },
 };
 
