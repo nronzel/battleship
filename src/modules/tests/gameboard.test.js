8fc78705efea721041c6d98f2bd433a98946ebea
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-23 13:53:31 -0500

    refactor: refactored tests
    made use of describe and beforeEach to prevent the redudant creation of
    a gameboard in each test
    
    modified the tests to better test object creation

--- src/modules/tests/gameboard.test.js
+++ src/modules/tests/gameboard.test.js
@@ -1,74 +1,94 @@
 import { Gameboard } from "../gameboard.js";
 
-test("tests board object", () => {
-  let board = Gameboard();
+describe("Gameboard", () => {
+  let gameboard;
 
-  expect(board.allSunk).toBe(false);
+  beforeEach(() => {
+    gameboard = Gameboard();
+  });
 
-  expect(board).toHaveProperty("initGrid");
-});
+  test("Gameboard should create an object with correct properties", () => {
+    expect(gameboard).toMatchObject({
+      misses: expect.any(Array),
+      hits: expect.any(Array),
+      allSunk: expect.any(Boolean),
+      grid: expect.any(Array),
+      ships: expect.any(Array),
+      receiveAttack: expect.any(Function),
+      checkIfHit: expect.any(Function),
+      randomMove: expect.any(Function),
+      checkIfExistingMove: expect.any(Function),
+      allShipsSunk: expect.any(Function),
+      initGrid: expect.any(Function),
+      setShipLocation: expect.any(Function),
+    });
+  });
 
-test("verifies board gets created", () => {
-  let board = Gameboard();
-  board.initGrid();
+  test("Verify an array of 5 ships gets created", () => {
+    expect(gameboard.ships).toHaveLength(5);
+  });
 
-  expect(board.grid).toHaveLength(10);
-});
+  test("All ships sunk is true after 17 total hits", () => {
+    let hits = [
+      [1, 2],
+      [1, 3],
+      [1, 4],
+      [1, 5],
+      [1, 6],
+      [1, 7],
+      [1, 8],
+      [1, 9],
+      [2, 2],
+      [2, 3],
+      [2, 4],
+      [2, 5],
+      [2, 6],
+      [2, 7],
+      [2, 8],
+      [2, 9],
+      [0, 0],
+    ];
 
-test("all ships sunk is true after 17 total hits", () => {
-  let board = Gameboard();
-  let hits = [
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [1, 5],
-    [1, 6],
-    [1, 7],
-    [1, 8],
-    [1, 9],
-    [2, 2],
-    [2, 3],
-    [2, 4],
-    [2, 5],
-    [2, 6],
-    [2, 7],
-    [2, 8],
-    [2, 9],
-    [0, 0],
-  ];
-  board.hits.push(...hits);
-  board.allShipsSunk();
-  expect(board.allSunk).toBe(true);
-  expect(board.hits).toHaveLength(17);
-});
+    gameboard.hits.push(...hits);
+    gameboard.allShipsSunk();
 
-test("register misses - no duplicates", () => {
-  let board = Gameboard();
-  board.setShipLocation(board.ships[4], [
-    [3, 3],
-    [4, 3],
-  ]);
+    expect(gameboard.allSunk).toBe(true);
+    expect(gameboard.hits).toHaveLength(17);
+  });
 
-  board.receiveAttack([1, 1]);
-  board.receiveAttack([1, 1]);
-  board.receiveAttack([1, 2]);
+  test("Receive attack and return 'not hit' if missed", () => {
+    const result = gameboard.receiveAttack([0, 0]);
 
-  expect(board.misses).toHaveLength(2);
-  expect(board.hits).toHaveLength(0);
-});
+    expect(result).toBe("not hit");
+    expect(gameboard.misses).toContainEqual([0, 0]);
+  });
 
-test("register hits - no duplicates", () => {
-  let board = Gameboard();
-  board.setShipLocation(board.ships[4], [
-    [3, 3],
-    [4, 3],
-  ]);
+  test("Receive attack and return 'hit' if ship is hit", () => {
+    // place ship at [0,0]
+    gameboard.setShipLocation(gameboard.ships[4], [
+      [0, 0],
+      [0, 1],
+    ]);
 
-  board.receiveAttack([3, 3]);
-  board.receiveAttack([3, 3]);
-  board.receiveAttack([1, 2]);
-  board.receiveAttack([1, 2]);
+    const result = gameboard.receiveAttack([0, 0]);
 
-  expect(board.misses).toHaveLength(1);
-  expect(board.hits).toHaveLength(1);
+    expect(result).toBe("hit");
+    expect(gameboard.hits).toContainEqual([0, 0]);
+  });
+
+  test("generates random coordinate", () => {
+    let move = gameboard.randomMove();
+
+    expect(Array.isArray(move)).toBe(true);
+    expect(move.length).toBe(2);
+  });
+
+  test("random coordinate is in playable area", () => {
+    let move = gameboard.randomMove();
+
+    expect(move[0]).toBeLessThan(10);
+    expect(move[0]).toBeGreaterThan(0);
+    expect(move[1]).toBeLessThan(10);
+    expect(move[1]).toBeGreaterThan(0);
+  });
 });
