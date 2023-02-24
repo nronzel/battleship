6227da24cbb6d6464dbfbba059628e08cb0c2ed5
nronzel <86695181+nronzel@users.noreply.github.com>
2023-02-24 11:58:08 -0500

    add: ui module
    also added functions to generate the player boards

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,3 +1,24 @@
 import { Ship } from "./ship.js";
 import { Player } from "./player.js";
 import { Gameboard } from "./gameboard.js";
+
+const createGrid = (container) => {
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      const gridItem = document.createElement("div");
+      gridItem.setAttribute("data-x", j);
+      gridItem.setAttribute("data-y", i);
+      container.appendChild(gridItem);
+    }
+  }
+};
+
+const createPlayerBoards = () => {
+  const playerBoard = document.getElementById("Player");
+  const cpuBoard = document.getElementById("Cpu");
+
+  createGrid(playerBoard);
+  createGrid(cpuBoard);
+};
+
+export { createPlayerBoards };
